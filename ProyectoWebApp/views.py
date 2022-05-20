2d4887679926c976a440ffd31868642396ebc062
miguelborda <miguelangelbg20@gmail.com>
2022-05-20 18:45:19 -0400

    5to C35: mostrando servicios en la pagina

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
+from servicios.models import Servicio
+
 # Create your views here.
 
 def home(request):
@@ -8,7 +10,8 @@ def home(request):
 
 def servicios(request):
 
-    return render(request, "ProyectoWebApp/servicios.html")
+    servicios=Servicio.objects.all()
+    return render(request, "ProyectoWebApp/servicios.html",{"servicios": servicios})
 
 def tienda(request):
 
