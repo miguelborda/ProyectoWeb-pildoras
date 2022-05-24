732753b5e1afbf0570d12b23311b695fe22e1e5c
miguelborda <miguelangelbg20@gmail.com>
2022-05-23 21:12:33 -0400

    14,C44: AppContacto: formato al formulario, css, csrf_token para seguridad de envío

--- contacto/forms.py
+++ contacto/forms.py
@@ -5,4 +5,4 @@ class FormularioContacto(forms.Form):
 
     email=forms.CharField(label="Email", required=True)
     
-    contenido=forms.CharField(label="Contenido")
+    contenido=forms.CharField(label="Contenido", widget=forms.Textarea)
