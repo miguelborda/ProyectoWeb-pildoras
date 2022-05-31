2cdef6cf3aa57b86ed4171f13b1c70821b1050d6
miguelborda <miguelangelbg20@gmail.com>
2022-05-30 21:43:03 -0400

    23,C53: appCarro: funciones agregar, eliminar, guardar...

--- carro/carro.py
+++ carro/carro.py
@@ -1,3 +1,6 @@
+from pickle import TRUE
+
+
 class Carro:
     def __init__(self, request):
         self.request=request
@@ -16,5 +19,34 @@ class Carro:
                 "precio":str(producto.precio),
                 "cantidad":1,
                 "imagen":producto.imagen.url
-
             }
+        else:
+            for key, value in self.carro.items():
+                if key==str(producto.id):
+                    value["cantidad"]=value["cantidad"]+1
+                    break
+        self.guardar_carro()
+
+    def guardar_carro(self):
+        self.session["carro"]=self.carro
+        self.session.modified=True
+
+    def eliminar(self, producto):
+        producto.id=str(producto.id)
+        if producto.id in self.carro:
+            del self.carro[producto.id]
+            self.guardar_carro()
+
+    def restar_producto(self, producto):
+        for key, value in self.carro.items():
+            if key==str(producto.id):
+                value["cantidad"]=value["cantidad"]-1
+                if value["cantidad"]<1:
+                    self.eliminar(producto)
+                break
+        self.guardar_carro()
+
+    def limpiar_carro(self):
+        self.session["carro"]={}
+        self.session.modified=True
+
