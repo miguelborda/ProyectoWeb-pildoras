746de08c5fd5db63aee0c7c29342e6341fa61e93
miguelborda <miguelangelbg20@gmail.com>
2022-06-08 14:27:17 -0400

    28,C58: appCarro: widget ajustes

--- carro/carro.py
+++ carro/carro.py
@@ -24,6 +24,7 @@ class Carro:
             for key, value in self.carro.items():
                 if key==str(producto.id):
                     value["cantidad"]=value["cantidad"]+1
+                    value["precio"]=float(value["precio"])+producto.precio
                     break
         self.guardar_carro()
 
@@ -41,6 +42,7 @@ class Carro:
         for key, value in self.carro.items():
             if key==str(producto.id):
                 value["cantidad"]=value["cantidad"]-1
+                value["precio"]=float(value["precio"])-producto.precio
                 if value["cantidad"]<1:
                     self.eliminar(producto)
                 break
