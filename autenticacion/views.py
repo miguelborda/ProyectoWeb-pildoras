8e87efab266107564bff775a6b786126b669e123
miguelborda <miguelangelbg20@gmail.com>
2022-06-09 21:34:16 -0400

    32,C62: appAutenticacion: crispy forms, registro de usuarios

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import login 
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,4 +14,15 @@ class VRegistro(View):
         
 
     def post(self, request):
-        pass
+        form=UserCreationForm(request.POST)
+
+        if form.is_valid():
+            usuario=form.save()
+            login(request, usuario)
+            return redirect('Home')
+
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+            
+            return render(request,"registro/registro.html",{"form":form})
--- carro/carro.py
+++ carro/carro.py
@@ -3,7 +3,7 @@ from pickle import TRUE
 
 class Carro:
     def __init__(self, request):
-        self.request=request
+        '''self.request=request
         self.session=request.session
         carro=self.session.get("carro")
         if not carro:
